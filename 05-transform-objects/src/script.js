2d619755371179e28ce8d6ebf817b49f71bfdd35
Prabhatlodhi <lodhi.prabhat@gmail.com>
2023-05-24 06:11:04 +0530

    aa

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -9,31 +9,58 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+
+//below when he started taught group
+
+const group = new THREE.Group()
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: "green"})
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: "yellow"})
+)
+group.add(cube2)
+cube2.position.x = -2
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: "red"})
+    )
+group.add(cube3)
+cube3.position.x = -1.2
 
 
-//Positioning Object
-// mesh.position.x = 0.7
-// mesh.position.y = -0.6
-// mesh.position.z = 1
-//if want to do all at one
-mesh.position.set(0.7, -0.6, 1)
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+// scene.add(mesh)
 
-//Scale
-// mesh.scale.x = 1.2
-// mesh.scale.y = 0.2
-// mesh.scale.z = 0.7
-//alternate of above
-mesh.scale.set(2,0.5,0.5)
 
-//rotate-rotation
-mesh.rotation.reorder('YXZ')
-mesh.rotation.x = Math.PI * 0.25 
-mesh.rotation.y = Math.PI * 0.25
-// mesh.rotation.z = 1
+// //Positioning Object
+// // mesh.position.x = 0.7
+// // mesh.position.y = -0.6
+// // mesh.position.z = 1
+// //if want to do all at one
+// mesh.position.set(0.7, -0.6, 1)
+
+// //Scale
+// // mesh.scale.x = 1.2
+// // mesh.scale.y = 0.2
+// // mesh.scale.z = 0.7
+// //alternate of above
+// mesh.scale.set(2,0.5,0.5)
+
+// //rotate-rotation
+// mesh.rotation.reorder('YXZ')
+// mesh.rotation.x = Math.PI * 0.25 
+// mesh.rotation.y = Math.PI * 0.25
+// // mesh.rotation.z = 1
 
 
 //Axes Helper
@@ -57,12 +84,14 @@ const sizes = {
  * Camera
 */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-console.log(mesh.position.distanceTo(camera.position))
+// console.log(mesh.position.distanceTo(camera.position))
 camera.position.z = 3
 // camera.position.y = 1
 // camera.position.x = 1
 scene.add(camera)
 
+// camera.lookAt(new THREE.Vector3(3,0,0))
+// camera.lookAt(mesh.position)
 
 
 
