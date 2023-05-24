fff40910db70f3e3909b4482155eb7127cb5e40f
Prabhatlodhi <lodhi.prabhat@gmail.com>
2023-05-24 07:50:27 +0530

    aa

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import gsap from 'gsap'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -28,4 +29,41 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+//Clock
+// const clock = new THREE.Clock()
+
+//GSAP lib
+gsap.to(mesh.position, {duration:1, delay:1, x:2})
+gsap.to(mesh.position, {duration:1, delay:2, x:0})
+
+// let time = Date.now()
+//Animations
+const tick = ()=>{
+    //Clock
+    // const elapsedTime = clock.getElapsedTime()
+    //Time
+    // const currentTime = Date.now()
+    // const deltaTime = currentTime - time;
+    // time = currentTime
+    //Update the cube
+    // mesh.rotation.y += 0.001 * deltaTime
+    // mesh.rotation.y += 0.001 
+    // mesh.rotation.y = elapsedTime 
+
+    //mesh is moving
+    // mesh.position.y = Math.sin(elapsedTime) 
+    // mesh.position.x = Math.cos(elapsedTime) 
+
+    //camera is moving
+    // camera.position.y = Math.sin(elapsedTime) 
+    // camera.position.x = Math.cos(elapsedTime) 
+    //do not use get delta
+    // camera.lookAt(mesh.position)
+
+    //Render
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
+}
+
+tick()
