9dd0fbf153803fe8fc1798717032ecaf8d8d3ab2
Prabhatlodhi <lodhi.prabhat@gmail.com>
2023-05-25 05:38:06 +0530

    aa

--- 08-fullscreen-and-resizing/src/script.js
+++ 08-fullscreen-and-resizing/src/script.js
@@ -22,9 +22,31 @@ scene.add(mesh)
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 }
+
+window.addEventListener('resize',()=>{
+    sizes.width = window.innerWidth,
+    sizes.height = window.innerHeight
+
+    //updated camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    //update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
+
+})
+
+window.addEventListener('dblclick',()=>{
+    if(!document.fullscreenElement){
+        canvas.requestFullscreen()
+    }else{
+        document.exitFullscreen()
+    }
+})
 
 /**
  * Camera
@@ -45,6 +67,7 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
 
 /**
  * Animate
